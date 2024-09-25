7c996e8e11963fca4389e2dc3d15f03d714a34d6
Lgrebol <lgrebol08@boscdelacoma.cat>
2024-09-25 18:33:44 +0200

    Not finished code in AuthenticationService.java, but two methods created

--- src/AuthenticationService.java
+++ src/AuthenticationService.java
@@ -1,3 +1,15 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class AuthenticationService {
-    
+    private Map<String, User> users = new HashMap<>();
+
+    public void registerUser(String nickname, String password) {
+        users.put(nickname, new User(nickname, password));
+    }
+
+    public boolean login(String nickname, String password) {
+        User user = users.get(nickname);
+        return user != null && user.getPassword().equals(password);
+    }
 }
