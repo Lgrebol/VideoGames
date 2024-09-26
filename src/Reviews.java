c56a432157d1a4551fd9bc0f538f13db182a1868
Lgrebol <lgrebol08@boscdelacoma.cat>
2024-09-26 15:57:34 +0200

    Reviews file created and with the getters and setters implemented

--- src/Reviews.java
+++ src/Reviews.java
@@ -1,3 +1,28 @@
 public class Reviews {
-    
+    private User user;
+    private int rating;
+    private String comment;
+
+    public Reviews(User user, int rating, String comment) {
+        this.user = user;
+        this.rating = rating;
+        this.comment = comment;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void showReview() {
+        System.out.println(user.getNickname() + " rated: " + rating + "/5");
+        System.out.println("Comment: " + comment);
+    }
 }
